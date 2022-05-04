21eae0abcd946c3dc8462be5e7b452899ac39522
CaraMcAvinchey <cara.mcavinchey@gmail.com>
2022-05-04 13:11:46 +0000

    Add array of icons from game controls section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// This game was built using code from Mehdi Aoussiad from: https: //javascript.plainenglish.io/building-a-rock-paper-scissors-game-with-javascript-bce23d39509d
+
 // Setting the scores and selecting our HTML elements.
 let computerScore = 1;
 let playerScore = 1;
@@ -6,77 +8,8 @@ const cScore = document.getElementById('computerScore');
 const buttons = document.querySelectorAll('.selection button');
 const showIcon = document.querySelector('.show i');
 const computerShowIcon = document.querySelector('.computer i');
+
 // The randomClass array below this contains the rock,paper, and scissor Icon from font-awesome.
-const randomClasses = ["fas fa-hand-rock", "fas fa-hand-paper","fas fa-hand-scissors"];
+const randomClasses = ["fas fa-hand-rock", "fas fa-hand-paper", "fas fa-hand-scissors", "fas fa-hand-lizard", "fas fa-hand-spock"];
 const text = document.getElementById('demo');
 const text2 = document.getElementById('demo2');
-
-// Game Functionality: Setting forEach function for the buttons.
-const game = () =>{
-    buttons.forEach(btn =>{
-        btn.addEventListener('click',(e)=>{
-        // Random rock paper scissor for the computer and the player
-           let clickedBtn = e.target.className;
-           showIcon.className = clickedBtn;
-           let randomNum = Math.floor(Math.random() * randomClasses.length);
-           computerShowIcon.className = randomClasses[randomNum];
-           // Game Score.
-           // If it's a Tie .
-           if(showIcon.className === computerShowIcon.className){
-               pScore.innerHTML = pScore.innerHTML;
-               cScore.innerHTML = cScore.innerHTML;
-               text.innerHTML = "It's a Tie ! ";
-               text.style.color = 'orange';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'orange';
-           } 
-          // if it's not a Tie.
-           else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[2]){
-               pScore.innerHTML = playerScore;
-               playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
-           }else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[1]){
-               cScore.innerHTML = computerScore;
-               computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[2]){
-               cScore.innerHTML = computerScore;
-               computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[0]){
-               pScore.innerHTML = playerScore;
-               playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
-           }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[0]){
-               cScore.innerHTML = computerScore;
-               computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[1]){
-               pScore.innerHTML = playerScore;
-               playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
-           }
-        });
-    });
-}
-// Calling the function.
-game();
-// This function contains all the game logic.
